1d6a0d2f149525cb58985a1c4c877c6e87689243
Sumanbk007 <bksuman1211@gmail.com>
2024-04-17 20:31:51 +0545

    Voting functionality added

--- routes/candidateRoutes.js
+++ routes/candidateRoutes.js
@@ -7,19 +7,6 @@ const User = require("./../models/User");
 
 const { jwtAuthMiddleware } = require("./../jwt");
 
-// const checkAdminRole = async (userId) => {
-//   try {
-//     const user = await User.findById(userId);
-//     console.log(user);
-
-//     if (user.role === "admin") {
-//       return true;
-//     }
-//   } catch (err) {
-//     return false;
-//   }
-// };
-
 const checkAdminRole = async (userID) => {
   try {
     const user = await User.findById(userID);
@@ -113,6 +100,81 @@ router.delete("/:candidateID", jwtAuthMiddleware, async (req, res) => {
     res.status(200).json(response);
   } catch (err) {
     res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+//Voting logic
+
+router.get("/vote/:candidateID", jwtAuthMiddleware, async (req, res) => {
+  candidateID = req.params.candidateID;
+  userId = req.user.id;
+
+  try {
+    // Find the Candidate document with  candidateID
+    const candidate = await Candidate.findById(candidateID);
+    if (!candidate) {
+      return res.status(404).json({ message: "Candidate not found" });
+    }
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "user not found" });
+    }
+    if (user.role == "admin") {
+      return res.status(403).json({ message: "admin is not allowed" });
+    }
+    if (user.isVoted) {
+      return res.status(400).json({ message: "You have already voted" });
+    }
+
+    // Update the Candidate document to record the vote
+    candidate.votes.push({ user: userId });
+    candidate.voteCount++;
+    await candidate.save();
+
+    // update the user document
+    user.isVoted = true;
+    await user.save();
+
+    return res.status(200).json({ message: "Vote recorded successfully" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// vote count
+router.get("/vote/count", async (req, res) => {
+  try {
+    // Find all candidates and sort them by voteCount in descending order
+    const candidate = await Candidate.find().sort({ voteCount: "desc" });
+
+    // Map the candidates to only return their name and voteCount
+    const voteRecord = candidate.map((data) => {
+      return {
+        party: data.party,
+        count: data.voteCount,
+      };
+    });
+
+    return res.status(200).json(voteRecord);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Get List of all candidates
+router.get("/", async (req, res) => {
+  try {
+    // Find all candidates and select only the name and party fields
+    const candidates = await Candidate.find({}, "name party -_id");
+
+    // Return the list of candidates
+    res.status(200).json(candidates);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
